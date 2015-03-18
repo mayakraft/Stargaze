c79076a2b143cccbe47481ba981552a0fad88d8c
robbykraft <robbykraft@gmail.com>
2015-03-18 00:25:36 -0500

    verified sidereal time for all time zones

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # Stargaze
 
-input your orientation (+Z orthogonal to ground, +X compass north (true north)) and gives you a rotation matrix that is your difference from the ecliptic plane (+Z orthogonal, +X RA0), given the time and day and your location on Earth.
+input your orientation on Earth (+Z orthogonal to ground, +X compass north (true north)) 
+
+outputs a rotation matrix that is your difference from the ecliptic plane (+Z orthogonal, +X RA0), given the time and day and your location on Earth.
 
 these are the missing set of functions required to build stargazing apps on mobile devices
--- stargaze.c
+++ stargaze.c
@@ -18,18 +18,18 @@ double mod360(double input){
 
 // ASTRONOMY
 
+// time since january 1st, at 12:00 (noon)
 double UTCDaysSinceJ2000(int year, int month, int day, int hour, int minute, int second){
-	int wholePart = 367 * year - floor(7 * (year + floor(month+9)/12.0)/4.0) 
-				+ floor(275 * month/9.0) + day - 730531.5; 
+	double wholePart = 367*year-floor(7*(year+floor((month+9)/12.0))/4.0)+floor(275*month/9.0)+day-730531.5;
 	double fractionalPart = (hour + minute/60.0 + second/3600.0)/24.0;
-	return wholePart + fractionalPart;
+	return (double)wholePart + fractionalPart;
 }
 
-double daysSinceJ2000(int timeZone, int year, int month, int day, int hour, int minute, int second){
-	return UTCDaysSinceJ2000(year, month, day, hour + timeZone, minute, second);
-}
+// double daysSinceJ2000(int timeZone, int year, int month, int day, int hour, int minute, int second){
+// 	return UTCDaysSinceJ2000(year, month, day, hour + timeZone, minute, second);
+// }
 
-void meanSiderealTime(double J2000Day, double longitude){
+double greenwichMeanSiderealTime(double J2000Day){
 	// In 10 years, the approximation error will be 0.001 seconds. 30
 	// years from now the error will still be better than 0.01 seconds.
 	// A century from now, the approximation error will be 0.1 seconds.
@@ -45,13 +45,17 @@ void meanSiderealTime(double J2000Day, double longitude){
 	// not dynamical time, known as TDT. Formulas used to predict the
 	// positions of planets, the Moon, eclipses and so on use TDT. 
 
-	double greenwichMeanSiderealTime = mod360(280.46061837 + 360.98564736629 * J2000Day + LTR);
+	return mod360(280.46061837 + 360.98564736629 * J2000Day + LTR);
+}
 
-	double localMeanSiderealTime = mod360(280.46061837 + 360.98564736629 * J2000Day + LTR + longitude);
+// J2000 time must be from GMT time zone
+double localMeanSiderealTime(double J2000Day, double longitude){
+	double LTR = 0.000388 * pow(J2000Day/36525.0, 2);
+	return mod360(280.46061837 + 360.98564736629 * J2000Day + LTR + longitude);
 }
 
 // incorporates the wobble caused by the moon's pull, the barycenter is not at the center of the Earth
-void apparentSiderealTime(double J2000Day){
+double apparentSiderealTime(double J2000Day){
 	double t = J2000Day / 36525.0;
 	// GMST = greenwichMeanSiderealTime();
 
@@ -63,17 +67,77 @@ void apparentSiderealTime(double J2000Day){
 	double dp = -17.2*sin(Om) -1.32*sin(2*L) -0.23*sin(2*L1) +0.21*sin(2*Om);
 	double de =   9.2*cos(Om) +0.57*cos(2*L) + 0.1*cos(2*L1) -0.09 *cos(2*Om);
 	// e = eps + de;
-	 e = dp + de;
+	e = dp + de;
 
 	// double correction = dp * cos(e) / 15;
 	double correction = dp * cos(e) / 3600.0;
+	return correction;
 }
-
 
 int main(){
-	double J2000 = UTCDaysSinceJ2000(2015, 3, 10, 12, 33, 10);
-	meanSiderealTime(J2000, -8);
-	apparentSiderealTime(J2000);
-	printf("%f\n",J2000);
+	int year, month, day, hour, minute, second;
+	year = 2015;
+	month = 3;
+	day = 18;
+	hour = 5;
+	minute = 20;
+	second = 0;
+	// input year in UTC time
+	double J2000 = UTCDaysSinceJ2000(year, month, day, hour, minute, second);
+	// double sidereal = greenwichMeanSiderealTime(J2000);
+	double sidereal = localMeanSiderealTime(J2000, -97.73);
+	double apparent = apparentSiderealTime(J2000);
+	printf("%f\t(%d/%d/%d)\t(%d:%2d:%2d) S:%f  A:%f\n", J2000, month, day, year, hour, minute, second, sidereal, apparent);
+
+
+
+	// for(int i = 0; i < 24; i++){
+	// 	int month = 2;
+	// 	int day = 21;
+	// 	int hour = i;
+	// 	double J2000 = UTCDaysSinceJ2000(2015, month, day, hour, 0, 0);
+	// 	double sidereal = greenwichMeanSiderealTime(J2000);
+	// 	double apparent = apparentSiderealTime(J2000);
+	// 	printf("(%d/%d/2015)\t(%f)\t(%d:00):%f, %f\n",month + 1, day + 1, J2000, hour, sidereal, apparent);
+	// }
+
+	// printf("\n\n\n");
+
+	// for(int i = 0; i < 30; i++){
+	// 	int month = 2;
+	// 	int day = i;
+	// 	int hour = 0;
+	// 	double J2000 = UTCDaysSinceJ2000(2015, month, day, hour, 0, 0);
+	// 	double sidereal = greenwichMeanSiderealTime(J2000);
+	// 	double apparent = apparentSiderealTime(J2000);
+	// 	printf("(%d/%d/2015)\t(%f)\t(%d:00):%f, %f\n",month + 1, day + 1, J2000, hour, sidereal, apparent);
+	// }
+	// printf("\n\n\n");
+
+	// for(int i = 0; i < 12; i++){
+	// 	int month = i;
+	// 	int day = 21;
+	// 	int hour = 0;
+	// 	double J2000 = UTCDaysSinceJ2000(2015, month, day, hour, 0, 0);
+	// 	double sidereal = greenwichMeanSiderealTime(J2000);
+	// 	double apparent = apparentSiderealTime(J2000);
+	// 	printf("(%d/%d/2015)\t(%f)\t(%d:00):%f, %f\n",month + 1, day + 1, J2000, hour, sidereal, apparent);
+	// }
+
+	// printf("\n\n\n");
+
+	// int minute = 50;
+	// for(int i = 0; i < 10; i++){
+	// 	int month = 3;//rand()%12;
+	// 	int day = i+1;//rand()%28;
+	// 	int hour = 12;//rand()%24;
+	// 	minute -= 4;
+	// 	double J2000 = UTCDaysSinceJ2000(2015, month, day, hour, minute, 0);
+	// 	double sidereal = greenwichMeanSiderealTime(J2000);
+	// 	double apparent = apparentSiderealTime(J2000);
+	// 	printf("(%d/%d/2015)\t(%f)\t(%d:%d):%f, %f\n",month + 1, day + 1, J2000, hour, minute, sidereal, apparent);
+	// }
+	
 	return 0;
 }
+
